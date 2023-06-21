72602441d90040e2c202c6695e2160daff336f63
Alingof <Alignof@outlook.com>
2023-06-21 16:28:18 +0900

    [update] step 1 has been completed

--- src/main.s
+++ src/main.s
@@ -3,7 +3,8 @@
 .STRING_INVALID_ARGUMENT: .string "invalid arguments\n"
 .STRING_EPILOGUE: .string ".intel_syntax noprefix\n.globl main\n"
 .STRING_LABEL_MAIN: .string "main:\n"
-.STRING_MOV_ARGV1_TO_RAX: .string "\tmov rax, %d\n"
+.STRING_MOV_ARGV1_TO_RAX: .string "\tmov rax, "
+.STRING_LINEBREAK: .string "\n"
 .STRING_RET: .string "\tret\n"
 .text
 .globl main
@@ -52,9 +53,35 @@ main:
     mov rax, 1  # write syscall
     syscall
 
-    # write(1, "\tmov rax, %d\n", 13);
-    mov rdx, 13 # len("\tmov rax, %d\n")
+    # write(1, "\tmov rax, ", 11);
+    mov rdx, 11 # len("\tmov rax, ")
     lea rsi, .STRING_MOV_ARGV1_TO_RAX[rip] # string 
+    mov rdi, 1  # stdout
+    mov rax, 1  # write syscall
+    syscall
+
+    # write(1, argv[1], 2);
+    mov rsi, [rbp - 0x10] # char **argv 
+    mov rsi, [rsi] # char *argv
+    add rsi, 8  # argv + 1
+    # seek null charactor and count length
+    mov rdx, rsi
+.BEGIN_COUNT:
+    mov al, [rdx]
+    cmp al, 0
+    je .END_COUNT
+    add rdx, 1
+    jmp .BEGIN_COUNT
+.END_COUNT:    
+
+    sub rdx, rsi    # len
+    mov rdi, 1  # stdout
+    mov rax, 1  # write syscall
+    syscall
+
+    # write(1, "\n", 1);
+    mov rdx, 1 # len("\n")
+    lea rsi, .STRING_LINEBREAK[rip] # string 
     mov rdi, 1  # stdout
     mov rax, 1  # write syscall
     syscall
