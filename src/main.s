43c2875f98d46f842ee1ac44708e3f2ab5d54276
Alingof <Alignof@outlook.com>
2023-07-06 17:29:31 +0900

    [update] add an argument number to comment out

--- src/main.s
+++ src/main.s
@@ -30,10 +30,10 @@ main:
         je .ARGC_IS_2
 
         # write(2, "invalid arguments\n", 18);
-        mov rdx, LEN_INVALID_ARGUMENT # len("invalid arguments\n")
-        lea rsi, .STRING_INVALID_ARGUMENT[rip] # string 
+        mov rdx, LEN_INVALID_ARGUMENT # 3rd: len("invalid arguments\n")
+        lea rsi, .STRING_INVALID_ARGUMENT[rip] # 2nd: string 
         mov rdi, 2  # stderr
-        mov rax, 1  # write syscall
+        mov rax, 1  # syscall num: write syscall
         syscall
 
         # return 1;
@@ -46,24 +46,24 @@ main:
     #}
 
     # write(1, ".intel_syntax noprefix\n.globl main\n", 35);
-    mov rdx, LEN_PROLOGUE # len(".intel_syntax noprefix\n.globl main\n")
-    lea rsi, .STRING_PROLOGUE[rip] # string 
-    mov rdi, 1  # stdout
-    mov rax, 1  # write syscall
+    mov rdx, LEN_PROLOGUE # 3rd: len(".intel_syntax noprefix\n.globl main\n")
+    lea rsi, .STRING_PROLOGUE[rip] # 2nd: string 
+    mov rdi, 1  # 1st: stdout
+    mov rax, 1  # syscall num: write syscall
     syscall
 
     # write(1, "main:\n", 6);
-    mov rdx, LEN_LABEL_MAIN  # len("main:\n")
-    lea rsi, .STRING_LABEL_MAIN[rip] # string 
-    mov rdi, 1  # stdout
-    mov rax, 1  # write syscall
+    mov rdx, LEN_LABEL_MAIN  # 3rd: len("main:\n")
+    lea rsi, .STRING_LABEL_MAIN[rip] # 2nd: string 
+    mov rdi, 1  # 1st: stdout
+    mov rax, 1  # syscall num: write syscall
     syscall
 
     # write(1, "\tmov rax, ", 11);
-    mov rdx, LEN_MOV_ARGV1_TO_RAX # len("\tmov rax, ")
-    lea rsi, .STRING_MOV_ARGV1_TO_RAX[rip] # string 
-    mov rdi, 1  # stdout
-    mov rax, 1  # write syscall
+    mov rdx, LEN_MOV_ARGV1_TO_RAX # 3rd: len("\tmov rax, ")
+    lea rsi, .STRING_MOV_ARGV1_TO_RAX[rip] # 2nd: string 
+    mov rdi, 1  # 1st: stdout
+    mov rax, 1  # syscall num: write syscall
     syscall
 
     # write(1, argv[1], 2);
@@ -82,23 +82,23 @@ main:
     .END_COUNT:    
     #}
 
-    sub rdx, rsi    # len = seek - &argv[1]
-    mov rdi, 1  # stdout
-    mov rax, 1  # write syscall
+    sub rdx, rsi    # 3rd: len = seek - &argv[1]
+    mov rdi, 1  # 1st: stdout
+    mov rax, 1  # syscall num: write syscall
     syscall
 
     # write(1, "\n", 1);
-    mov rdx, LEN_LINEBREAK # len("\n")
-    lea rsi, .STRING_LINEBREAK[rip] # string 
-    mov rdi, 1  # stdout
-    mov rax, 1  # write syscall
+    mov rdx, LEN_LINEBREAK # 3rd: len("\n")
+    lea rsi, .STRING_LINEBREAK[rip] # 2nd: string 
+    mov rdi, 1  # 1st: stdout
+    mov rax, 1  # syscall num: write syscall
     syscall
 
     # write(1, "\tret\n", 5);
-    mov rdx, LEN_RET # len("\tret\n")
-    lea rsi, .STRING_RET[rip] # string 
-    mov rdi, 1  # stdout
-    mov rax, 1  # write syscall
+    mov rdx, LEN_RET # 3rd: len("\tret\n")
+    lea rsi, .STRING_RET[rip] # 2nd: string 
+    mov rdi, 1  # 1st: stdout
+    mov rax, 1  # syscall num: write syscall
     syscall
 
     # return 0;
